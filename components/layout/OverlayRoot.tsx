908a26b79c22e07c1ebca5f15bb16ca4126b78dc
Resi Respati <resir014@gmail.com>
2020-01-05 17:15:16 +0700

    fixed reset styles

--- components/layout/OverlayRoot.tsx
+++ components/layout/OverlayRoot.tsx
@@ -1,11 +1,14 @@
 import * as React from 'react'
-import { reset, Box } from 'components/ui-core'
+import { reset, global, Box } from 'components/ui-core'
 
 const OverlayRoot: React.SFC = ({ children }) => (
   <Box display="flex" flexDirection="column" width="100%" height="100%" minHeight="100vh" overflow="hidden">
     {children}
     <style jsx global>
       {reset}
+    </style>
+    <style jsx global>
+      {global}
     </style>
   </Box>
 )
--- pages/overlay/alerts.tsx
+++ pages/overlay/alerts.tsx
@@ -1,182 +1,142 @@
+/* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable react/no-danger */
 import * as React from 'react'
 import { NextPage } from 'next'
 import Head from 'next/head'
 import convert from 'htmr'
+import css from 'styled-jsx/css'
 
 import { OverlayRoot } from 'components/layout'
 
 //#region test CSS
 
-const css = `
-* {
-  box-sizing: border-box;
-}
+const overlayStyles = css.global`
+  html,
+  body {
+    font-size: 16px;
+    line-height: 1.45;
+    height: 100%;
+    width: 100%;
+    margin: 0px;
+    padding: 0px;
+    overflow: hidden;
+  }
 
-html {
-  box-sizing: inherit;
-}
+  html {
+    box-sizing: border-box;
+  }
 
-html,
-body {
-  font-family: 'Lato', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu,
-    Cantarell, 'Fira Sans', 'Droid Sans', 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  line-height: 1.45;
-  height: 100%;
-  width: 100%;
-  margin: 0px;
-  padding: 0px;
-  overflow: hidden;
-}
+  *,
+  *::before,
+  *::after {
+    box-sizing: inherit;
+  }
 
-.widget-AlertBox {
-  position: relative;
-}
+  .widget-AlertBox {
+    position: relative;
+  }
 
-#alert-box {
-  height: 100%;
-  width: 100%;
-  position: absolute;
-}
+  .ck-alert-wrapper {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    position: relative;
+    height: 100%;
+    width: 100%;
+    padding: 8px;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Fira Sans', 'Droid Sans',
+      'Helvetica Neue', Arial, sans-serif;
+  }
 
-#alert-box.hidden,
-.hidden {
-  opacity: 0;
-}
+  .ck-alert-wrapper__inner {
+    display: flex;
+    flex: 1 1 auto;
+    flex-direction: column;
+    width: 100%;
+  }
 
-#wrap {
-  position: relative;
-  height: 100%;
-  width: 100%;
-}
+  .ck-alert {
+    background-color: #2d2d34;
+    border-radius: 8px;
+    box-shadow: rgba(0, 0, 0, 0.25) 0px 4px 8px 0px;
+    text-align: center;
+    overflow: hidden;
+  }
 
-.ck-alert-wrapper {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  position: relative;
-  height: 100%;
-  width: 100%;
-  padding: 8px;
-}
+  .ck-alert:not(:first-child) {
+    margin-top: 20px;
+  }
 
-.ck-alert-wrapper__inner {
-  display: flex;
-  flex: 1 1 auto;
-  flex-direction: column;
-  width: 100%;
-}
+  .ck-alert__header {
+    padding: 10px 20px;
+  }
 
-.ck-alert {
-  background-color: #2d2d34;
-  border-radius: 8px;
-  box-shadow: rgba(0, 0, 0, 0.25) 0px 4px 8px 0px;
-  text-align: center;
-  overflow: hidden;
-}
+  .ck-alert--donation .ck-alert__header {
+    background-color: #0076ff;
+  }
 
-.ck-alert:not(:first-child) {
-  margin-top: 20px;
-}
+  .ck-alert__header h2 {
+    margin: 0;
+    text-transform: uppercase;
+    letter-spacing: 0.25px;
+    color: #fff;
+    font-size: 32px;
+    text-shadow: 0px 0px 1px #000, 0px 0px 2px #000, 0px 0px 3px #000, 0px 0px 4px #000, 0px 0px 5px #000;
+  }
 
-.ck-alert__header {
-  padding: 10px 20px;
-}
+  .ck-alert-text {
+    text-align: center;
+    color: #fff;
+    font-size: 24px;
+  }
 
-.ck-alert--streamlabs-prime .ck-alert__header {
-  background-color: #caa368;
-}
+  .ck-alert-text__inner {
+    display: block !important;
+    padding: 20px;
+    text-shadow: 0px 0px 1px #000, 0px 0px 2px #000, 0px 0px 3px #000, 0px 0px 4px #000, 0px 0px 5px #000;
+  }
 
-.ck-alert--raid .ck-alert__header {
-  background-color: #ff369a;
-}
+  .ck-alert-image {
+    display: flex;
+    flex: 1 1 auto;
+    justify-content: center;
+  }
+  .ck-alert-image__inner {
+    position: relative;
+    width: 100%;
+    height: 100%;
+  }
+  .ck-alert-image__inner video {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+  .ck-alert-image {
+    z-index: 6;
+    position: relative;
+  }
 
-.ck-alert--cheer .ck-alert__header {
-  background-color: #9b45ff;
-}
-
-.ck-alert--host .ck-alert__header {
-  background-color: #2234ce;
-}
-
-.ck-alert--donation .ck-alert__header {
-  background-color: #0076ff;
-}
-
-.ck-alert--follow .ck-alert__header {
-  background-color: #00bf80;
-}
-
-.ck-alert--sub .ck-alert__header {
-  background-color: #f5a623;
-}
-
-.ck-alert__header h2 {
-  margin: 0;
-  text-transform: uppercase;
-  letter-spacing: 0.25px;
-  color: #fff;
-  font-size: 32px;
-  text-shadow: 0px 0px 1px #000, 0px 0px 2px #000, 0px 0px 3px #000, 0px 0px 4px #000,
-    0px 0px 5px #000;
-}
-
-.ck-alert-text {
-  text-align: center;
-  color: #fff;
-  font-size: 24px;
-}
-
-.ck-alert-text__inner {
-  display: block !important;
-  padding: 20px;
-  text-shadow: 0px 0px 1px #000, 0px 0px 2px #000, 0px 0px 3px #000, 0px 0px 4px #000,
-    0px 0px 5px #000;
-}
-
-.ck-alert-image {
-  display: flex;
-  flex: 1 1 auto;
-  justify-content: center;
-}
-.ck-alert-image__inner {
-  position: relative;
-  width: 100%;
-  height: 100%;
-}
-.ck-alert-image__inner video {
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  top: 0;
-  left: 0;
-}
-.ck-alert-image {
-  z-index: 6;
-  position: relative;
-}
-
-#alert-message,
-#alert-user-message {
-  text-align: center;
-}
-#alert-user-message img {
-  vertical-align: middle;
-  height: 1em;
-}
-#alert-message > span > span {
-  display: inline-block;
-}
-#alert-text {
-  z-index: 6;
-  position: relative;
-}
-#alert-text-wrap {
-  z-index: 6;
-  position: relative;
-}
-
+  #alert-message,
+  #alert-user-message {
+    text-align: center;
+  }
+  #alert-user-message img {
+    vertical-align: middle;
+    height: 1em;
+  }
+  #alert-message > span > span {
+    display: inline-block;
+  }
+  #alert-text {
+    z-index: 6;
+    position: relative;
+  }
+  #alert-text-wrap {
+    z-index: 6;
+    position: relative;
+  }
 `
 
 //#endregion
@@ -250,9 +210,11 @@ const AlertsOverlayPage: NextPage = () => {
     <OverlayRoot>
       <Head>
         <title>Overlay</title>
-        <style dangerouslySetInnerHTML={{ __html: css }} />
       </Head>
       {result}
+      <style jsx global>
+        {overlayStyles}
+      </style>
     </OverlayRoot>
   )
 }
